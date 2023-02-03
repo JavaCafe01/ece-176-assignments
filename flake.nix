0f932969ae5649a3e00334f18cf4c4317ae62c97
Gokul Swaminathan <gokulswamilive@gmail.com>
2023-02-03 04:10:43 -0800

    Added a flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "ECE 140a Python Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        python = "python39";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (pkgs.${python}.withPackages
+              (ps: with ps; [ pip setuptools black numpy matplotlib urllib3 scikitimage jupyterlab ]))
+            pkgs.nodePackages.pyright
+          ];
+        };
+      });
+}
