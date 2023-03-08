60203b16bc47734650b0fc953c613b52ef9a040b
Gokul Swaminathan <gokulswamilive@gmail.com>
2023-03-08 00:31:51 -0800

    new assignment

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ECE 176 Python Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,11 +12,15 @@
         python = "python39";
         pkgs = nixpkgs.legacyPackages.${system};
       in
-      {
+      rec {
+        packages = {
+          dev-shell = devShell.inputDerivation;
+        };
+
         devShell = pkgs.mkShell {
           buildInputs = [
             (pkgs.${python}.withPackages
-              (ps: with ps; [ pip setuptools black numpy matplotlib urllib3 scikitimage jupyterlab ]))
+              (ps: with ps; [ pip setuptools black numpy matplotlib urllib3 scikitimage torch torchvision-bin jupyterlab ]))
             pkgs.nodePackages.pyright
           ];
         };
